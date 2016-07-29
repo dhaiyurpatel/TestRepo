25bfa32bbfa3bbc26c35bff821be74b5ad57c61a
Dhaiyur <makavana.dhaiyur@gmail.com>
2016-07-29 11:44:03 +0530

    Login and Splashscreen issue solved.
    Login and Splashscreen issue solved.

---
+++ app/src/main/java/info/androidhive/materialdesign/activity/HomeFragment.java
@@ -0,0 +1,64 @@
+package info.androidhive.materialdesign.activity;
+
+/**
+ * Created by Ravi on 29/07/15.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.androidhive.materialdesign.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        rootView.findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                PrefManager prefManager = new PrefManager(getActivity());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                Intent i = new Intent(getActivity(), WelcomeActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
